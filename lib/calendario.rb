ce13b9174808090ac67ded17773ba6cebd4af4a2
Wesley Oliveira <waeoliveira@gmail.com>
2010-09-07 10:31:04 -0300

    Método calendario agora deixa livre o usuário para criar seus dados

--- lib/calendario.rb
+++ lib/calendario.rb
@@ -1,6 +1,14 @@
 # Calendario
 module Calendario
-  def calendario(year,month,model,atribute=nil)
+  def calendario(year,month,action,eventos)
+    
+    h_eventos = Hash.new
+    eventos.each do |evento|
+      h_eventos[evento[0].to_date.strftime('%d-%m-%Y').to_s] = Array.new if h_eventos[evento[0].strftime('%d-%m-%Y').to_s].blank?
+      h_eventos[evento[0].to_date.strftime('%d-%m-%Y').to_s] << [evento[1],evento[2],evento[3],evento[0].strftime('%H:%M')]
+    end
+    logger.info h_eventos.inspect
+    
     month = month.to_i
     year  = year.to_i
     
@@ -12,27 +20,30 @@ module Calendario
        month = 12
     end
     
-    str = "<div class='calendario_header' align='center'><span style='float: left;'><a href='/#{model.to_s.downcase.pluralize}/#{year.to_i}_#{month.to_i - 1}/listar'><img src='/images/anterior.png'/></a></span>#{Date.civil(year.to_i,month.to_i).strftime('%B')} - #{Date.civil(year.to_i,month.to_i).strftime('%Y')}<span style='float: right;'><a href='/#{model.to_s.downcase.pluralize}/#{year.to_i}_#{month.to_i + 1}/listar'><img src='/images/proximo.png'/></a></span></div>"
+    str = "<div class='calendario_header' align='center'><span style='float: left;'><a href='http://#{request.host}:#{request.port}/#{action}/#{year.to_i}_#{month.to_i - 1}/listar'><img src='/images/anterior.png'/></a></span>#{Date.civil(year.to_i,month.to_i).strftime('%B')} - #{Date.civil(year.to_i,month.to_i).strftime('%Y')}<span style='float: right;'><a href='http://#{request.host}:#{request.port}/#{action}/#{year.to_i}_#{month.to_i + 1}/listar'><img src='/images/proximo.png'/></a></span></div>"
     str += "<table class='calendario'  cellspacing='0px' cellpading='0px'>"
     str += "<tr valign='middle'><th>Segunda</th><th>Terça</th><th>Quarta</th><th>Quinta</th><th>Sexta</th><th>Sábado</th><th>Domingo</th></tr>"
     
     str += "<tr class='line_calendario'>"
     Date.civil(year.to_i,month.to_i).at_beginning_of_month.at_beginning_of_week.upto(Date.civil(year.to_i,month.to_i).end_of_month) do |day|
       if(day.wday == 0)
-        logger.info "#{day.strftime('%d-%m-%Y')} #{Time.now.strftime("%d-%m-%Y")}"
-        str += "<td style='width: 14%;'><div  id='#{((day.to_date == Date.today) ? 'today' : '')}' class='row_calendario'><div class='topo'>#{day.day}</div>#{adicionar_evento_calendario(day.day,month,year,model,atribute)}</div></div></td></tr><tr class='line_calendario'>"
+        str += "<td style='width: 14%;'><div  id='#{((day.to_date == Date.today) ? 'today' : '')}' class='row_calendario'><div class='topo'>#{day.day}</div>#{adicionar_evento_calendario(format('%0.2d',day.day),format('%0.2d',month),year,h_eventos,action)}</div></div></td></tr><tr class='line_calendario'>"
       else
-        str += "<td style='width: 14%;'><div id='#{((day.to_date == Date.today) ? 'today' : '')}' class='row_calendario'><div class='topo'>#{day.day}</div>#{adicionar_evento_calendario(day.day,day.month,day.year,model,atribute)}</div></td>"
+        str += "<td style='width: 14%;'><div id='#{((day.to_date == Date.today) ? 'today' : '')}' class='row_calendario'><div class='topo'>#{day.day}</div>#{adicionar_evento_calendario(format('%0.2d',day.day),format('%0.2d',month),day.year,h_eventos,action)}</div></td>"
       end
     end
     str += "</tr></table><script type='text/javascript'></script>"
   end
   
-  def adicionar_evento_calendario(day,month,year,model,atribute)
+  def adicionar_evento_calendario(day,month,year,hash,action)
     str = ""
-    eval((model.to_s.singularize.camelize)).find(:all,:conditions=>["to_char(#{atribute.to_s},'yyyy-mm-dd') = '#{year}-#{format('%02d',month)}-#{format('%02d',day)}'"],:order => atribute.to_s).each do |evento|
-      str += "<div class='evento'><a href='/events/#{evento.id}' class='event_link' title='#{evento.descricao}'>#{evento.start_at.strftime('%H:%M')} - #{evento.name}</a></div>"
+
+    unless hash["#{day}-#{month}-#{year}"].blank?
+      hash["#{day}-#{month}-#{year}"].each do |evento|
+        str += "<div class='evento'><a href='/#{action}/#{evento[2]}' class='event_link' title='#{evento[1]}'>#{evento[3]} - #{evento[0]}</a></div>"
+      end
     end
+
     str
   end
 end
